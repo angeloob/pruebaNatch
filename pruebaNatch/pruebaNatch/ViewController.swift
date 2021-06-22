f933244cf9ff50e0ccf2bf5a896681e663a2a061
Angel Obed Olvera Borjas <angelolvera@live.com.mx>
2021-06-21 22:08:18 -0500

    creacion de proyecto

---
+++ pruebaNatch/pruebaNatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pruebaNatch
+//
+//  Created by Angel Olvera on 21/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
